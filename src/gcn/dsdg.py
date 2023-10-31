ca9061055f76d37f02f7d5df52512f31f5d466dc
Hongjian Zhou <39885479+MZhouke@users.noreply.github.com>
2023-10-31 19:50:41 -0400

    added dsdg

--- src/gcn/dsdg.py
+++ src/gcn/dsdg.py
@@ -24,8 +24,8 @@ class DSDGGenerator:
         for _, row in df.iterrows():
             disease = row['Disease']
             drug = row['Drug']
-            dsdg_dict[disease] = {f"Disease {cat}": row[f"Disease {cat}"] for cat in ['description', 'symptoms', 'causes', 'diagnosis', 'treatment', 'complications']}
-            dsdg_dict[drug] = {f"Drug {cat}": row[f"Drug {cat}"] for cat in ['description', 'dosage', 'effects', 'toxicity', 'food_interaction', 'drug_interaction', 'pharmacodynamics']}
+            dsdg_dict[disease] = {f"Disease {cat}": row[f"Disease {cat}"] for cat in ['symptoms', 'causes', 'diagnosis', 'treatment', 'complications']}
+            dsdg_dict[drug] = {f"Drug {cat}": row[f"Drug {cat}"] for cat in ['description', 'dosage', 'effects', 'toxicity', 'food_interaction', 'drug_interaction', 'pharmacodynamics', 'experimental_results']}
         return dsdg_dict
 
     def get_knowledge_category(self, name, category):
@@ -81,7 +81,3 @@ class DSDGGenerator:
     def get_graph(self):
         return self.G
 
-# Example usage
-excel_path = "your_file.xlsx"
-dsdg = DSDGGenerator(excel_path)
-G = dsdg.get_graph()
