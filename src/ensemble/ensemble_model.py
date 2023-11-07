06c7787741cfa7c7e5d3225dc6e27d2c7c557606
Hongjian Zhou <39885479+MZhouke@users.noreply.github.com>
2023-11-06 22:19:00 -0500

    updated prompt manager and ensemble model

--- src/ensemble/ensemble_model.py
+++ src/ensemble/ensemble_model.py
@@ -1,1 +1,33 @@
+from prompt_manager import PromptManager  # Assuming PromptManager is in a separate module now
+from llama_utils import inference
+class EnsembleModel:
+    def __init__(self, prompt_manager, soft_prompt, knowledge_base):
+        self.prompt_manager = prompt_manager
+        self.soft_prompt = soft_prompt
+        self.knowledge_base = knowledge_base
+
+    def run_inference(self, input_data):
+        # Inquiry Analysis (IA) using the standard prompt
+        ia_sys_prompt = self.prompt_manager.generate_combined_prompt("inquiry_analysis", "sys_prompt")
+        ia_fs_prompt = self.prompt_manager.generate_combined_prompt("inquiry_analysis", "fs_prompt")
+        ia_response = inference(ia_sys_prompt, ia_fs_prompt, input_data)
+
+        # Knowledge Acquisition (KA) using Few-shot with soft prompts and DSDG knowledge
+        ka_sys_prompt = self.prompt_manager.generate_combined_prompt("knowledge_acquisition", "sys_prompt")
+        ka_fs_prompt = self.prompt_manager.generate_combined_prompt("knowledge_acquisition", "fs_prompt")
+        combined_ka_prompt = f"{self.soft_prompt}\n{ka_sys_prompt}\n{ka_fs_prompt}"
+        # The DSDG knowledge base is accessed here to enrich the KA step
+        dsdg_enriched_input = self.knowledge_base.access_knowledge(ia_response)
+        ka_response = inference(combined_ka_prompt, dsdg_enriched_input)
+
+        # Evidence Generation (EG) using the output from KA
+        eg_sys_prompt = self.prompt_manager.generate_combined_prompt("evidence_generation", "sys_prompt")
+        eg_fs_prompt = self.prompt_manager.generate_combined_prompt("evidence_generation", "fs_prompt")
+        eg_response = inference(eg_sys_prompt, eg_fs_prompt, ka_response)
+
+        return {
+            "ia_response": ia_response,
+            "ka_response": ka_response,
+            "eg_response": eg_response
+        }
 # Placeholder
--- src/prompt/prompt_manager.py
+++ src/prompt/prompt_manager.py
@@ -6,24 +6,43 @@ class PromptManager:
     def __init__(self):
         self.sys_prompts = {
             'inquiry_analysis': {
-                'task': "Your task is to identify any relevant drugs, symptoms, and diseases in the user's inquiry.",
+                'task': 'You are tasked with identifying the drug, symptom, and disease from user inquiry:Drugs: List '
+                        'any. If none, your answer for this section should be ’[]’. Answer should be in form ’[drug '
+                        'a, drug b, ...]’.Symptoms: List any. If none, your answer for this section should be ’[]’. '
+                        'Answer should be in form ’[symptom a, symptomb, ...]’.Diseases: List any. If none, '
+                        'your answer for this section should be ’[]’. Answer should be in form ’[disease a, '
+                        'disease b, ...].',
                 'answer_format': "Question: {question}\nAnswer: Drugs {drugs}, Symptoms {symptoms}, Disease {disease}"
             },
             'knowledge_acquisition': {
-                'task': "Your task is to identify the knowledge base numbers needed to answer the question.",
+                'task': 'Task: You are tasked with extracting the knowledge to answer a medical inquiry accurately. '
+                        'Step 1: Identify the categories of knowledge needed (List the numbers corresponding to the '
+                        'knowledge categories necessary) to answer the inquiry correctly. If none, your answer for '
+                        'this section should be ’[]’. Answer should be in form ’[1, 2, 3, ...]’.The knowledge '
+                        'categories of drugs are:1. Drug description and indication.2. Drug dosage recommendation.3. '
+                        'Drug adverse effect.4. Drug toxicity.5. Drug-food interaction.6. Drug-drug interaction.7. '
+                        'Drug pharmacodynamics.8. Pubmed experimental summaries.The knowledge categories of diseases '
+                        'and symptoms are:1. Common symptoms.2. Disease causes.3. Disease diagnosis.4. Disease '
+                        'treatment.5. Disease complications.Step 2: Extract the specific knowledge from the '
+                        'identified knowledge categories to answer the inquiry correctly.',
                 'answer_format': "Question: {question}\nIdentified: Drugs {drugs}, Symptoms {symptoms}, Disease {"
                                  "disease}\nAnswer: Knowledge Needed {knowledge_needed}"
             },
             'evidence_generation': {
-                'task': "Your task is to answer multiple-choice questions. Understand the question, analyze it, "
-                        "and provide a concise and accurate answer.",
-                'answer_format': """
-Analysis: Provide an analysis that logically leads to the answer based on the relevant information.
-Final Answer: Provide the final answer, which should be a single letter in the alphabet representing the best option among the multiple choices provided in the question."""
+                'task': """Your task is to answer questions. Understand the question, analyze it step by step, 
+                and provide a concise and accurate answer. Among the provided choices, choose the one that best fits 
+                the criteria below:TO DO:Only use the knowledge provided to answer the inquiryNOT TO DO: 1. Do not 
+                make assumptions not supported by the provided content. 2. Avoid providing personal opinions or 
+                interpretations. 3. Summarize and interpret the knowledge provided objectively and accurately.""",
+                'answer_format': """Analysis: Provide an analysis that logically leads to the answer based on the 
+                relevant information. Final Answer: Provide the final answer, which should be a single letter in the 
+                alphabet representing the best option among the multiple choices provided in the question. When 
+                analyzing each choice, include the relevant knowledge relied upon and display its source link (
+                provided as Link[https://...]) to the relevant part of your output"""
             }
         }
         self.fs_examples = {
-            'inquiry_analysis': [
+            'inquiry_analysis':
                 {
                     'question': "A 29-year-old woman develops painful swelling of both hands. She is also very stiff "
                                 "in the morning. Physical examination reveals involvement of the proximal "
@@ -35,21 +54,60 @@ Final Answer: Provide the final answer, which should be a single letter in the a
                                  "involvement of proximal interphalangeal joints and metacarpophalangeal joints."],
                     'diseases': ["None"]
                 }
-            ],
-            'knowledge_acquisition': [
+            ,
+            'knowledge_acquisition':
                 {
-                    'question': "A 29-year-old woman develops painful swelling of both hands. She is also very stiff "
-                                "in the morning. Physical examination reveals involvement of the proximal "
-                                "interphalangeal joints and metacarpophalangeal (MCP) joints. Her RF is positive and "
-                                "ANA is negative. Which of the following medications is most likely to improve her "
-                                "joint pain symptoms?",
-                    'drugs': ["D-penicillamine", "an anti-malarial", "methotrexate", "NSAID or aspirin"],
-                    'symptoms': ["Painful swelling of both hands", "stiffness in the morning",
-                                 "involvement of proximal interphalangeal joints and metacarpophalangeal joints."],
-                    'diseases': ["None"],
-                    'knowledge_needed': [1, 8]
-                }
-            ]
+                    'question': """A 29-year-old woman develops painful swelling of both hands. She is also very stiff 
+                                in the morning. Physical examination reveals involvement of the proximal 
+                                interphalangeal joints and metacarpophalangeal (MCP) joints. Her RF is positive and 
+                                ANA is negative. Which of the following medications is most likely to improve her 
+                                joint pain symptoms?
+                                Drugs Identified: ["D-penicillamine", "an anti-malarial", "methotrexate", "NSAID or aspirin"]
+                                Symptoms Identified: ["Painful swelling of both hands", "stiffness in the morning",
+                                 "involvement of proximal interphalangeal joints and metacarpophalangeal joints."]
+                                 Diseases Identified: ["None"]""",
+                    'knowledge_needed': "[1, 8]",
+                },
+            'evidence_generation': {
+                'task': """Question: A 29-year-old woman develops painful swelling of both hands. She is also very 
+                stiff in the morning. Physical examination reveals involvement of the proximal interphalangeal joints 
+                and metacarpophalangeal (MCP) joints. Her RF is positive and ANA is negative. Which of the following 
+                medications is most likely to improve her joint pain symptoms? Knowledge: Key embedding: 
+                D-penicillamine Knowledge Block: Penicillamine is a chelating (KEE-late-ing) agent that binds to 
+                excess copper and removes it from the blood stream. Penicillamine is used to remove excess copper in 
+                people with an inherited condition called Wilson's disease. Penicillamine is also used to treat 
+                severe rheumatoid arthritis after other medicines have been tried without success. Penicillamine is 
+                not approved to treat juvenile rheumatoid arthritis. Link[
+                https://www.drugs.com/mtm/penicillamine.html] Key embedding: Anti-malarial Knowledge Block: 
+                Hydroxychloroquine is a quinoline medicine used to treat or prevent malaria, a disease caused by 
+                parasites that enter the body through the bite of a mosquito. Hydroxychloroquine is also used to 
+                treat symptoms of rheumatoid arthritis and discoid or systemic lupus erythematosus. Link[
+                https://www.drugs.com/hydroxychloroquine.html] Key embedding: Methotrexate Knowledge Block: 
+                Methotrexate interferes with the growth of certain cells of the body, especially cells that reproduce 
+                quickly, such as cancer cells, bone marrow cells, and skin cells. Methotrexate is used to treat 
+                leukemia and certain types of cancer of the breast, skin, head and neck, lung, or uterus. 
+                Methotrexate is also used to treat severe psoriasis and rheumatoid arthritis in adults. It is also 
+                used to treat active polyarticular-course juvenile rheumatoid arthritis in children. Link[
+                https://www.drugs.com/methotrexate.html] Key embedding: NSAID or aspirin Knowledge Block: Aspirin, 
+                a salicylate, is used for immediate relief of pain, fever, inflammation, arthritis, migraines, 
+                and reduce the risk of major adverse cardiovascular events. It provides relief for various symtoms 
+                such as the flu, the common cold, neck and back pain, rheumatoid arthritis, bursitis, burns, 
+                and various injuries. Link[https://www.drugs.com/aspirin.html] Key Embedding: ['Painful swelling of 
+                both hands', 'stiffness in the morning', 'involvement of proximal interphalangeal joints and 
+                metacarpophalangeal joints.']  Knowledge Block: rheumatoid arthritis: Rheumatoid arthritis is a 
+                long-term condition that causes pain, swelling and stiffness in the joints. The condition usually 
+                affects the hands, feet and wrists.Common medications: methotrexate, leflunomide, hydroxychloroquine, 
+                sulfasalazine. DMARD such as methotrexate is usually the first medicine given for rheumatoid 
+                arthritis, often with another NSAID to relieve any pain. DMARDs help slow the progress of RA, 
+                and reduce pain, stiffness, and inflammation, however, they do not provide short-term pain relief and 
+                may take several weeks or months to demonstrate a clinical effect. NSAIDs , such as aspirin, 
+                ibuprofen, are used to help decrease swelling, pain, and fever and relieve joint pain symptoms 
+                instead. Link[https://www.nhs.uk/conditions/rheumatoid-arthritis/] """,
+                'answer_format': """Analysis: Provide an analysis that logically leads to the answer based on the 
+                        relevant information. Final Answer: Provide the final answer, which should be a single letter in the 
+                        alphabet representing the best option among the multiple choices provided in the question."""
+            }
+
         }
 
     def generate_combined_prompt(self, task_type, prompt_type):
